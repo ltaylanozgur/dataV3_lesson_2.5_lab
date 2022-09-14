5df5c82e631cd81d113cbdfe147b633457a72b2f
ltaylanozgur <ltaylanozgur@gmail.com>
2022-09-15 00:00:07 +0300

    Ozgur_sql_lab

---
+++ sql_lab_2.5.sql
@@ -0,0 +1,71 @@
+USE sakila; -- Sakila database
+
+-- 1. Select all the actors with the first name ‘Scarlett’.
+SELECT * FROM sakila.actor 
+WHERE first_name = 'Scarlett';
+
+
+-- 2. How many films (movies) are available for rent and how many films have been rented?
+SELECT COUNT(DISTINCT(film_id)) as 'movies' 
+FROM inventory;
+
+
+-- 3. What are the shortest and longest movie duration? 
+-- Name the values max_duration and min_duration.
+SELECT min(length) as min_duration, max(length) as max_duration 
+FROM sakila.film;
+
+
+-- 4. What's the average movie duration expressed in format (hours, minutes)?
+SELECT CONCAT(LEFT(AVG(length)/60,1), ':' ,ROUND(AVG(length) % 60)) AS avg_movie_duration
+FROM film; -- after conversion
+
+-- 5. How many distinct (different) actors' last names are there?
+SELECT COUNT(DISTINCT last_name) 
+FROM sakila.actor;
+
+
+-- 6. Since how many days has the company been operating (check DATEDIFF() function)?
+SELECT rental_date(), DATEDIFF(day, (convert(max(rental_date), DATE)), (convert(min(rental_date), DATE)) AS number_of_days) 
+FROM sakila.rental;
+
+SELECT * , CONVERT(rental_date,DATE) AS rental_date2 FROM sakila.rental;
+SELECT * , CONVERT(rental_date,datetime) AS rental_date2 FROM sakila.rental;
+SELECT (CONVERT(max(rental_date)), (CONVERT(min(rental_date)) AS b)) FROM sakila.rental;
+
+
+-- 7. Show rental info with additional columns month and weekday. Get 20 results.
+SELECT *, month(CONVERT(rental_date, DATE)) AS month, weekday(convert(rental_date, DATE)) AS weekday
+FROM sakila.rental
+LIMIT 20;
+
+
+-- 8. Add an additional column day_type with values 'weekend' and 'workday' 
+-- depending on the rental day of the week.
+SELECT *, month(convert(rental_date, DATE)) as weekend, weekday(convert(rental_date, DATE)) AS weekday
+FROM sakila.rental;
+
+
+-- 9. Get release years.
+SELECT title, release_year FROM sakila.film; -- I just wanted to display the film titles as well.
+SELECT DISTINCT release_year FROM sakila.film; -- unique value
+
+
+-- 10. Get all films with ARMAGEDDON in the title.
+SELECT * FROM sakila.film  WHERE title LIKE 'ARMAGEDDON%';
+
+
+-- 11. Get all films which title ends with APOLLO.
+SELECT * FROM sakila.film WHERE title LIKE '%APOLLO';
+
+
+-- 12. Get 10 the longest films.
+SELECT * FROM sakila.film
+ORDER BY length DESC
+LIMIT 10;
+
+
+-- 13. How many films include Behind the Scenes content?
+SELECT COUNT(*) FROM sakila.film WHERE special_features like 'Behind the Scenes';
+
+
